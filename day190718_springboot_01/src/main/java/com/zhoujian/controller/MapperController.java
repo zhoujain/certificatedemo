49443bbc680a78df41a1d6298882bac1a99b6989
zhoujian <zhoujian0727@foxmail.com>
2019-07-21 21:09:36 +0800

    把本地工程添加版本库

--- day190718_springboot_01/src/main/java/com/zhoujian/controller/MapperController.java
+++ day190718_springboot_01/src/main/java/com/zhoujian/controller/MapperController.java
@@ -78,8 +78,8 @@ public class MapperController {
         lists.add(jstreeVO5);
 
         vo.setData(lists);
-        System.out.println(lists);
-        System.out.println(vo.toString());
+        //System.out.println(lists);
+        //System.out.println(vo.toString());
         return vo;
     }
 
