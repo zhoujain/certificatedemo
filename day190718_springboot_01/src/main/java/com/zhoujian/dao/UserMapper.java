d4c10145bc1e7ca78b92b280c7f5821928d6f711
zhoujian <zhoujian0727@foxmail.com>
2019-07-22 14:35:05 +0800

    添加Dao层User的功能，并编写测试类

--- day190718_springboot_01/src/main/java/com/zhoujian/dao/UserMapper.java
+++ day190718_springboot_01/src/main/java/com/zhoujian/dao/UserMapper.java
@@ -2,14 +2,54 @@ package com.zhoujian.dao;
 
 import com.zhoujian.domain.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
+
+
 @Mapper
 public interface UserMapper {
+    /**
+     * 查询所有用户
+     * @return
+     */
     List<User> queryUserList();
 
+    /**
+     * 根据用户名查询用户
+     * @param username
+     * @return
+     */
     User findUserByUsername(String username);
+
+    /**
+     * 添加用户
+     */
+    Boolean insertUser(User user);
+
+    /**
+     * 删除用户
+     */
+    Boolean deleteUser(Integer uid);
+
+    /**
+     * 根据uid查询用户
+     * @param uid
+     * @return
+     */
+    User findUserByUid(Integer uid);
+
+    /**
+     * 根据username查询密码
+     */
+    String findUpwdByUsername(String username);
+
+    /**
+     * 修改用户
+     */
+    Boolean updateUser(User user);
+
 }
 
 
